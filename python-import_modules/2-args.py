1017d9c71e4d70453bf14275d6fe67c3afc151cb
fitowashere <fito_washere@icloud.com>
2023-09-27 22:31:43 -0400

    hotfix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,17 @@
 if __name__ == "__main__":
     import sys
 
-arguments = sys.argv[1:]#argv
-num_arguments = len(arguments)#argc
+arguments = sys.argv[1:]
+num_arguments = len(arguments)
 
-if num_arguments >= 1:
+if num_arguments == 1:
+    print(num_arguments, "argument:", end="")
+if num_arguments > 1:
     print(num_arguments, "arguments:", end="")
 if num_arguments == 0:
-    print(num_arguments, "argument.", end="\n")
+    print(num_arguments, "arguments.", end="\n")
 else:
     print("")
 
-for i in range(0, num_arguments):
+for i in range(num_arguments):
     print("{}: {}".format(i+1, arguments[i]))
