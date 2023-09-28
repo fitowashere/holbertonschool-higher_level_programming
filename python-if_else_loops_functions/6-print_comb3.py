e7cce207a0db447c96ed697b0f94759de732fb0b
fitowashere <fito_washere@icloud.com>
2023-09-28 00:36:11 -0400

    deleting commits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,11 +2,8 @@
 # Use two loops to generate combinations of two digits
 for digit1 in range(0, 9):
     for digit2 in range(digit1 + 1, 10):
-        # Print the combination with a comma and space until digit1 gets to 8
-        # print("{:02d}".format(digit1 * 10 + digit2), end=', ' if digit1 < 8 else '') #
         if digit1 < 8:
             print("{:02d}".format(digit1 * 10 + digit2), end=', ')
         else:
             print("{:02d}".format(digit1 * 10 + digit2), end='')
-# Print a newline character to end the output
 print()
