00dc468695b9447b8a3a1e257d57343ebcca3b47
fitowashere <fito_washere@icloud.com>
2023-09-27 23:08:41 -0400

    task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+arguments = len(sys.argv)
+
+if arguments == 1:
+    print("0")
+
+else:
+    result = 0
+    for i in range(1, arguments):
+     result += int(sys.argv[i])
+
+    print(result)
