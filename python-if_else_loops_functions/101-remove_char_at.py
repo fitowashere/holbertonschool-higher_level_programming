9b25db242ad213ea89264763734fbb0075b5e332
fitowashere <fito_washere@icloud.com>
2023-09-28 00:24:50 -0400

    validation error

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):# prototype that was given
-    if n < 0 or n >= len(str):# checks if n is less than 0 or more than the amount of letters 
-        return str  # Return the original text if n is more than the amount of letters 
-    result = "" # initialize the an empty string
-    for i in range(len(str)): 
-        if i != n: # checks if the possition is not equil to n
-            result += str[i] # adds the letter to the empty string
-    return result # return the results
+def remove_char_at(str, n):
+    # prototype that was given
+    if n < 0 or n >= len(str):
+        # checks if n is less than 0 or more than the amount of letters
+        return str
+    # Return the original text if n is more than the amount of letters
+    result = ""
+    # initialize the an empty string
+    for i in range(len(str)):
+        if i != n:
+            # checks if the possition is not equil to n
+            result += str[i]
+            # adds the letter to the empty string
+    return result
+# return the results
