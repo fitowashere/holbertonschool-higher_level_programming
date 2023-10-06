54ae3139c70a2df6836c48a30594c675397d18bb
fitowashere <fito_washere@icloud.com>
2023-10-06 14:18:38 -0400

    bonus first try

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        # Attempt to format and print the value as an integer
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        # Print the exception message to stderr
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
+    
---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+import sys
+
+def safe_function(fct, *args):
+    try:
+        # Call the given function with the provided arguments
+        result = fct(*args)
+        return result
+    except Exception as e:
+        # Print the exception message to stderr
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    
