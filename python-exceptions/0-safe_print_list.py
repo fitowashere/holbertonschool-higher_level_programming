3491dc08fffb18e3ed1df9da38dc1d652a955e69
fitowashere <fito_washere@icloud.com>
2023-10-06 13:21:43 -0400

    hot fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-        print()
-        return i + 1
-    except:
+    except IndexError:
         print()
         return i
+    else:
+        print()
+        return x
+    
