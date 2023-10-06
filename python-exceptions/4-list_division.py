215e14031e640225f14044c4f471c6a154ffb12c
fitowashere <fito_washere@icloud.com>
2023-10-06 13:37:29 -0400

    Pycodestyle validation

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,12 +3,18 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
+
+            # Raise an IndexError if the index is out of range
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError("out of range")
+
             # Try to divide corresponding elements, if they exist
             num1 = my_list_1[i] if i < len(my_list_1) else 0
             num2 = my_list_2[i] if i < len(my_list_2) else 0
 
             # Check for division by 0 and wrong type
-            if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
+            if (not isinstance(num1, (int, float)) or
+                not isinstance(num2, (int, float))):
                 raise TypeError("wrong type")
             if num2 == 0:
                 raise ZeroDivisionError("division by 0")
