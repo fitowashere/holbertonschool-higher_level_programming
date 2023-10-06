c9cf9804f1d4dc1f9479fc9555e9bab6de30229d
fitowashere <fito_washere@icloud.com>
2023-10-06 11:59:46 -0400

    typo

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
+    r_d = {'I': 1, 'V': 5, 'X': 10,
                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            sum += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        if i > 0 and r_d[roman_string[i]] > r_d[roman_string[i - 1]]:
+            sum += r_d[roman_string[i]] - 2 * r_d[roman_string[i - 1]]
         else:
-            sum += roman_dict[roman_string[i]]
+            sum += r_d[roman_string[i]]
     return sum
