1cd70796fe32ed1102f47f9af99f8285d3c1120c
fitowashere <fito_washere@icloud.com>
2023-10-13 13:37:05 -0400

    bonus third time

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -3,37 +3,68 @@
 
 
 class Node:
-    """A class to represent a Node."""
+    """Class that defines a node of a singly linked list."""
+
     def __init__(self, data, next_node=None):
+        """
+        Initializes a Node.
+        
+        Parameters:
+            data: The data of the node, must be an integer.
+            next_node: The next node in the linked list.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Gets the data of the node."""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Sets the data of the node.
+        
+        Raises:
+            TypeError: If data is not an integer.
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """Gets the next node."""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Sets the next node.
+        
+        Raises:
+            TypeError: If next_node is not a Node object or None.
+        """
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+    """Class that defines a singly linked list."""
+
     def __init__(self):
+        """Initializes a singly linked list with no nodes."""
         self.__head = None
 
     def __str__(self):
+        """
+        Returns a string representation of the linked list with one node per line.
+        
+        Returns:
+            A string containing all nodes in the linked list.
+        """
         nodes = []
         current = self.__head
         while current is not None:
@@ -42,6 +73,12 @@ class SinglyLinkedList:
         return "\n".join(nodes)
 
     def sorted_insert(self, value):
+        """
+        Inserts a new Node into the correct sorted position in the list (increasing order).
+        
+        Parameters:
+            value: The data of the new node to be inserted.
+        """
         new_node = Node(value)
         if self.__head is None or self.__head.data >= value:
             new_node.next_node = self.__head
