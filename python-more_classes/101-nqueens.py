929fdfb5cd5c031b20c1b6456c65087505289d76
fitowashere <fito_washere@icloud.com>
2023-10-13 13:23:34 -0400

    bonus

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_solution(board):
+    solution = []
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if board[i][j] == 1:
+                solution.append([i, j])
+    print(solution)
+
+
+def is_safe(board, row, col, n):
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(board, row, n):
+    if row >= n:
+        print_solution(board)
+        return
+
+    for i in range(n):
+        if is_safe(board, row, i, n):
+            board[row][i] = 1
+            solve_nqueens(board, row + 1, n)
+            board[row][i] = 0
+
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+try:
+    N = int(sys.argv[1])
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
+
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+board = [[0 for _ in range(N)] for _ in range(N)]
+solve_nqueens(board, 0, N)
