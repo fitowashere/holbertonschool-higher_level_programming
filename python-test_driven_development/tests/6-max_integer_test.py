cb3bd1314dd9b8b64a6a7c7945d382f4dc3e9c69
fitowashere <fito_washere@icloud.com>
2023-10-13 11:50:03 -0400

    redo

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -16,7 +16,7 @@ class Test_max_integer(unittest.TestCase):
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([1]), 1)
         self.assertEqual(max_integer([1, 1]), 1)
-        self.assertRaises(TypeError, max_integer["hello"])
+        self.assertRaises(TypeError, max_integer(["hello"]))
         self.assertRaises(TypeError, max_integer())
 
 
