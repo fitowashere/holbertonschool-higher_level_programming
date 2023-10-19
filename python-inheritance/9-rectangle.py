1910ffb98d46a0f9551b89141af3045b7ed97bf8
fitowashere <fito_washere@icloud.com>
2023-10-19 13:46:42 -0400

    tasks

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,7 +16,16 @@ class Rectangle(BaseGeometry):
         """
         Function that validates the weight & height
         """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def print(self):
+        print(str(self))
