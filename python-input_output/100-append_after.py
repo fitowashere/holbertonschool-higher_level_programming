c651c87118746c8a2b2be3a9e3de4f45c1aadecc
fitowashere <fito_washere@icloud.com>
2023-10-20 11:57:57 -0400

    bonus test 2

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" append after"""
 def append_after(filename="", search_string="", new_string=""):
     """
     Inserts a line of text after each line 
--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,4 +1,5 @@
 #!/bin/usr/python3
+"""stats"""
 import sys
 import signal
 
