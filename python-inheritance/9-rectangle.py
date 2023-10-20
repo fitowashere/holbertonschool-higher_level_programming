d39ad819369837f41d295c75a220308d5d00bb0c
fitowashere <fito_washere@icloud.com>
2023-10-20 11:34:26 -0400

    hot fix

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""
-This module have a class called Rectangle that
-is a child of BaseGeometry
-"""
+"""Rectangle class inherited from BaseGeometry class"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Rectangle is a child class from BaseGeometry
-    """
+    """Rectangle class inherited from BaseGeometry class"""
     def __init__(self, width, height):
-        """
-        Function that validates the weight & height
-        """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """Rectangle class inherited from BaseGeometry class"""
         self.__width = width
         self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
 
     def area(self):
+        """Rectangle class inherited from BaseGeometry class"""
         return self.__width * self.__height
 
     def __str__(self):
+        """Rectangle class inherited from BaseGeometry class"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def print(self):
-        print(str(self))
