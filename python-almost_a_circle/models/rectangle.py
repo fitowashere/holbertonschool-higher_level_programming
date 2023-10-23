329b0181b9bee6cf310ab44f03decbd106988d81
fitowashere <fito_washere@icloud.com>
2023-10-23 12:16:24 -0400

    task 6 test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,8 +84,10 @@ class Rectangle(Base):
         """
         Print the Rectangle instance as a grid of '#' characters.
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
