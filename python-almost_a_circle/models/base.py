7efc9fe572805bd309b13212bb55675f3539b278
fitowashere <fito_washere@icloud.com>
2023-10-23 11:16:45 -0400

    task 0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""base module"""
+
+
+# models/base.py
+
+class Base:
+    """
+    This is the base class for your models.
+    """
+
+    # Private class attribute __nb_objects
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new instance of the Base class.
+        """
+        # Check if id is provided
+        if id is not None:
+            # Assign the provided id to the public instance attribute id
+            self.id = id
+        else:
+            # Increment __nb_objects and 
+            # assign it to the public instance attribute id
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
