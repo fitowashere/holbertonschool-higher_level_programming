786fbc9f6b7f27c87389f33a1afdfaf637f054f4
fitowashere <fito_washere@icloud.com>
2023-10-23 12:19:58 -0400

    task 8 test 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,3 +95,19 @@ class Rectangle(Base):
         """
         r = self
         return f"[Rectangle] ({r.id}) {r.x}/{r.y} - {r.width}/{r.height}"
+
+    def update(self, *args):
+        """
+        Update the attributes of the Rectangle instance with no-keyword
+        arguments in the specified order.
+        """
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
