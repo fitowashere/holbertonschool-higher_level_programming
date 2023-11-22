e1d5e583190d34e6a1ab6d920792e978311607eb
fitowashere <fito_washere@icloud.com>
2023-11-21 23:11:35 -0400

    typo

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,2 +1,4 @@
---script that removes all score less than 5-- Script to delete records from a table in a MySQL database based on a condition.
-DELETE FROM second_table WHERE schore <= 5;
+--script that removes all score less than 5
+
+DELETE FROM second_table
+WHERE score <= 5;
