51de7a2c08332825aa404194328058061f238c00
fitowashere <fito_washere@icloud.com>
2023-11-21 23:02:29 -0400

    testing

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,6 @@
---list the number with the same score
+-- Script to list the number of records with the same score in 'second_table'
+-- and sort the results by the count of records in descending order
+
 SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
