9c06248eb17f4a2f4c24be4e77e97e8061dd3089
fitowashere <fito_washere@icloud.com>
2023-11-21 22:56:36 -0400

    update code

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 --list the number with the same score
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
